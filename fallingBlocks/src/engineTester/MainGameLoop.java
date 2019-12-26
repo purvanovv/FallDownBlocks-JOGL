4b0df83a1c3bde4ddedf5649c6664644cb2985cb
Viktor Parvanov <victor.parvanov@nevexis.com>
2019-12-26 10:15:28 +0200

    Added second object to draw

--- fallingBlocks/src/engineTester/MainGameLoop.java
+++ fallingBlocks/src/engineTester/MainGameLoop.java
@@ -25,15 +25,30 @@ public class MainGameLoop {
 				 };
 
 		int[] indices = { 0, 1, 2, 1, 3, 2 };
+		
+		float[] vertices2 = {
+			-0.8f,-0.8f,0,
+			-0.8f,-0.4f,0,
+			-0.6f,-0.8f,0,
+			-0.6f,-0.4f,0
+		};
+		
+		int[] indices2 = {
+				0,1,2,2,1,3
+		};
 
 		RawModel rawModel = loader.loadToVAO(vertices, indices);
 		Entity entity = new Entity(rawModel, new Vector3f(0,0,0));
+		
+		RawModel rawModel2 = loader.loadToVAO(vertices2, indices2);
+		Entity entity2 = new Entity(rawModel2, new Vector3f(0,0,0));
 
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
 			entity.move();
 			shader.start();
 			renderer.render(entity,shader);
+			renderer.render(entity2,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
